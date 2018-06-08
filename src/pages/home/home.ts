81b947279fa034d34add894549acc04edc409056
harimasora <zidalo@gmail.com>
2018-06-08 12:22:45 -0300

    List of features on home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,42 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  pages = [
+    {
+      "title": "Download",
+      "location": null
+    }, {
+      "title": "Upload",
+      "location": null
+    }, {
+      "title": "Camera",
+      "location": null
+    }, {
+      "title": "Map & Location",
+      "location": null
+    }, {
+      "title": "Accelerometer & Gyro",
+      "location": null
+    }, {
+      "title": "Compass",
+      "location": null
+    }, {
+      "title": "Touch ID",
+      "location": null
+    }, {
+      "title": "Proximity Sensor",
+      "location": null
+    }, {
+      "title": "Bluetooth Connection",
+      "location": null
+    }]
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  pageSelected(page) {
+    console.log(page.title)
   }
 
 }
