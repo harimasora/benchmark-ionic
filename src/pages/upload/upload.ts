3179f5fba02b75597410e6c96cda581c899b8ac4
harimasora <zidalo@gmail.com>
2018-06-13 17:38:31 -0300

    Upload page

--- src/pages/upload/upload.ts
+++ src/pages/upload/upload.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { FileUploadOptions, FileTransfer, FileTransferObject } from '@ionic-native/file-transfer';
+import { File } from '@ionic-native/file';
+import { Platform, AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the UploadPage page.
@@ -14,11 +16,57 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class UploadPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  imagePath: string = 'assets/files/futurama.png';
+  fileEntry = null;
+  storageDirectory: string = '';
+
+  constructor(private platform: Platform, private transfer: FileTransfer, private file: File, private alertCtrl: AlertController) {
+    this.platform.ready()
+      .then(() => {
+        return this.file.resolveDirectoryUrl(this.file.applicationDirectory + 'www/assets/files')
+      })
+      .then((filesDir) => {
+        return this.file.getFile(filesDir, 'futurama.png', { create: false })
+      })
+      .then((fileEntry) => {
+        this.fileEntry = fileEntry;
+      })
+      .catch(error => {
+        console.error(JSON.stringify(error))
+      })
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad UploadPage');
+  upload() {
+    const url = 'https://content.dropboxapi.com/2/files/upload';
+
+    const fileTransfer: FileTransferObject = this.transfer.create();
+    let options: FileUploadOptions = {
+      headers: {
+        "Authorization": "Bearer frLGybnUFXoAAAAAAAAdaEEy48ISejZ4wspvf3r_syOqmV8FfH0Kxv_LUOv5Q6rn",
+        "Dropbox-API-Arg": "{\"path\": \"/TCC/uploaded-ionic.png\",\"mode\": \"add\",\"autorename\": false,\"mute\": false}",
+        "Content-Type": "application/octet-stream"
+      }
+    }
+    fileTransfer.upload(this.fileEntry.nativeURL, url, options)
+      .then((data) => {
+        // success
+        const alertSuccess = this.alertCtrl.create({
+          title: 'Upload Succeeded!',
+          subTitle: 'futurama.png was successfully uploaded to Dropbox',
+          buttons: ['Ok']
+        });
+
+        alertSuccess.present();
+      }, (error) => {
+        // error
+        const alertFailure = this.alertCtrl.create({
+          title: `Upload Failed!`,
+          subTitle: `futurama.png was not successfully uploaded. Error code: ${error.code}`,
+          buttons: ['Ok']
+        });
+
+        alertFailure.present();
+      })
   }
 
 }
